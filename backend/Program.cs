e8662360d43328536a5c207e2560530dd63400d1
Daniel Moreno Marquez <daniel-moreno-marquez@hotmail.es>
2023-03-24 08:58:08 +0200

    cors

--- backend/Program.cs
+++ backend/Program.cs
@@ -68,7 +68,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("react-policy", 
         builder =>
     {
-        builder.WithOrigins("http://localhost:5173")
+        builder.WithOrigins("http://localhost:5173", "http://localhost:3000")
         .AllowAnyMethod()
         .AllowAnyHeader()
         .SetIsOriginAllowedToAllowWildcardSubdomains();
