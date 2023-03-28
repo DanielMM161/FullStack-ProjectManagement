cef8d7e1105bac9a9308db5a3e57be5b856f34c7
Daniel Moreno Marquez <daniel-moreno-marquez@hotmail.es>
2023-03-28 21:47:58 +0300

    Responsive side bar

--- frontend/src/components/ListButtonItem/ListButtonItem.tsx
+++ frontend/src/components/ListButtonItem/ListButtonItem.tsx
@@ -1,17 +1,18 @@
-import { ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from '@mui/material';
 
 interface IButtonIconProps {
   title: string;
   onClick: () => void;
   children: React.ReactNode;
+  selected: boolean;
 }
 
-function ListButtonItem({ title, onClick, children }: IButtonIconProps) {
+function ListButtonItem({ title, onClick, children, selected }: IButtonIconProps) {  
   return (
-    <ListItem key="Dashboard" disablePadding onClick={() => onClick()}>
-      <ListItemButton>
-        <ListItemIcon>{children}</ListItemIcon>
-        <ListItemText primary={title} />
+    <ListItem key={title} disablePadding onClick={() => onClick()} sx={{ color: selected? 'green' : 'black' }} >
+      <ListItemButton color={selected ? 'green' : 'red'}>
+        <ListItemIcon sx={{ color: selected? 'green' : 'default' }}>{children}</ListItemIcon>      
+        <ListItemText primary={title} sx={{ fontWeight: 'bold', fontSize: '34px'}} />
       </ListItemButton>
     </ListItem>
   );
--- frontend/src/components/SideBar/index.tsx
+++ frontend/src/components/SideBar/index.tsx
@@ -1,50 +1,78 @@
 import { useNavigate } from 'react-router';
 import { Avatar, List, Typography } from '@mui/material';
 import DashboardIcon from '@mui/icons-material/Dashboard';
-import SettingsIcon from '@mui/icons-material/Settings';
 import LogoutIcon from '@mui/icons-material/Logout';
 import ListButtonItem from '../ListButtonItem/ListButtonItem';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux.hook';
 import StyledSideBar from './styled';
+import FolderIcon from '@mui/icons-material/Folder';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import { logOut } from '../../redux/slice/profile';
+import ExpandCircleDownIcon from '@mui/icons-material/ExpandCircleDown';
+import { useState } from 'react';
 
 function SideBar() {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const profileState = useAppSelector((state) => state.profile);
+  const [ dashboardItem, setDashboardItem] = useState(false);
+  const [ projectItem, setProjectItem] = useState(false);
+  const [ closeSideBar, setCloseSideBar] = useState(false);  
   const { profile } = profileState;
 
+
+  function handleDashboardClick() {
+    setDashboardItem(!dashboardItem)
+    setProjectItem(false)
+    navigate('/dashboard')
+  }
+
+  function handleProjectClick() {
+    setDashboardItem(false)
+    setProjectItem(!projectItem)
+  }
+
   return (
-    <StyledSideBar>
-      <div className="avatar-info">
-        <div className="info-name">
-          <Avatar alt={profile.firstName} src="/static/images/avatar/1.jpg" sx={{ width: 36, height: 36 }} />
-          <Typography variant="h6" sx={{ marginLeft: '8px' }}>
-            {profile.firstName} {profile.lastName}
-          </Typography>
+    <StyledSideBar className={closeSideBar ? 'close-side-bar glassmorphism' : 'open-side-bar'}>
+      <div className='top-side'>
+        <div className="avatar-info">
+          <div className="info-name">
+            <Avatar alt={profile.firstName} src="/static/images/avatar/1.jpg" sx={{ width: 56, height: 56 }} />
+            <KeyboardArrowLeftIcon  className='arrow-icon' onClick={() => setCloseSideBar(!closeSideBar)}/>
+            {closeSideBar ? (
+              <ExpandCircleDownIcon className='expand-icon' onClick={() => setCloseSideBar(!closeSideBar)} />
+            ) : null}
+          </div>
+            <Typography variant="h6">
+              {profile.firstName} {profile.lastName}
+            </Typography>
+            <Typography variant="subtitle1">
+              {profile.email}
+            </Typography>
+        </div>
+        <div className="list-container">
+          <List>
+            <ListButtonItem title="Dashboard" onClick={() => handleDashboardClick() } selected={dashboardItem}>
+              <DashboardIcon />
+            </ListButtonItem>
+            <ListButtonItem  title="Project" onClick={() => {handleProjectClick()}} selected={projectItem}>
+              <FolderIcon  />
+            </ListButtonItem>
+          </List>
         </div>
       </div>
-      <div className="list-container">
-        <List>
-          <ListButtonItem title="Dashboard" onClick={() => navigate('/dashboard')}>
-            <DashboardIcon />
-          </ListButtonItem>
-          <ListButtonItem title="Settings" onClick={() => {}}>
-            <SettingsIcon />
-          </ListButtonItem>
-        </List>
-      </div>
-      <div className="logout-container">
-        <List>
-          <ListButtonItem
-            title="Log out"
-            onClick={() => {
-              dispatch(logOut());
-            }}
-          >
-            <LogoutIcon />
-          </ListButtonItem>
-        </List>
+      <div className='bottom-side'>
+          <List>
+            <ListButtonItem
+              title="Log out"
+              onClick={() => {
+                dispatch(logOut());
+              }}
+              selected={false}
+            >
+              <LogoutIcon />
+            </ListButtonItem>
+          </List>        
       </div>
     </StyledSideBar>
   );
